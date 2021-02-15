3220a6d8ac0266da8d4ea56a4c52b55a24ae5b10
Johnshandsable <Jshands@asu.edu>
2021-02-15 15:39:33 -0600

    notes about react code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloCullen from './HelloCullen';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HelloCullen />
+        <h1>Go Home React, you're drunk!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/HelloCullen.jsx
@@ -0,0 +1,5 @@
+function HelloCullen() {
+  return <h1>Hello Cullen</h1>;
+}
+
+export default HelloCullen;
